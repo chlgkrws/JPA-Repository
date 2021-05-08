35463d805e7d6a332104ca48f95475c698eab9d6
chlgkrws <61148082+chlgkrws@users.noreply.github.com>
2021-05-09 00:40:17 +0900

    feat : Add JPA basic

---
+++ src/main/java/jpa/basic/JpaBasic.java
@@ -0,0 +1,50 @@
+package jpa.basic;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import jpa.basic.entity.Member;
+
+/**
+ * JPA 기본(조회, 삽입, 수정, 삭제)
+ * @author cgw981 2021.05.05
+ */
+public class JpaBasic {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa-application");
+		EntityManager em = emf.createEntityManager();
+
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+		try {
+			// 삽입
+			Member m = new Member();
+			m.setId(100L);
+			m.setName("춘식이");
+
+			// 조회
+			Member m2 = em.find(Member.class, 100L);
+
+			// 수정
+			m2.setName("수정된 춘식이");
+
+			// 삭제
+			em.remove(m2);
+
+			// JPQL
+			List<Member> result = em.createQuery("select m from Member as m", Member.class).getResultList();
+			System.out.println(result.get(0).getName());
+
+			tx.commit();
+		} catch (Exception e) {
+			tx.rollback();
+		}
+		em.close();
+		emf.close();
+	}
+}
