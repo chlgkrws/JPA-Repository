5b49f7c15e076e9e6c2813ee30869afcbb16f686
chlgkrws <61148082+chlgkrws@users.noreply.github.com>
2021-05-20 00:01:32 +0900

    feat : Add Inheritance structure & @MappedSuperclass

--- src/main/java/jpa/relation/JPAMain.java
+++ src/main/java/jpa/relation/JPAMain.java
@@ -1,14 +1,15 @@
 package jpa.relation;
 
-import java.util.List;
+import java.time.LocalDateTime;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
-import jpa.relation.entity.Member;
-import jpa.relation.entity.Team;
+import jpa.relation.entity.BaseEntity;
+import jpa.relation.entity.Item;
+import jpa.relation.entity.Movie;
 
 
 public class JPAMain {
@@ -20,27 +21,16 @@ public class JPAMain {
 		EntityTransaction tx = em.getTransaction();
 		tx.begin();
 		try {
-			Team team = new Team();
-			team.setId(150L);
-			team.setName("카카오");
-			em.persist(team);
-
-			Member m = new Member();
-			m.setId(100L);
-			m.setName("춘식이");
-			m.setTeam(team);
-			em.persist(m);
-
 			em.flush();
 			em.clear();
+			Movie movie = new Movie();
+			movie.setDirector("aaaa");
+			movie.setActor("bnbbb");
+			movie.setName("바람가");
+			movie.setPrice(10000);
+			movie.setCreatedTime(LocalDateTime.now());
 
-			Team findTeam  = em.find(Team.class, 150L);
-			Member findMember = em.find(Member.class, 100L);
-			List<Member> members =findMember.getTeam().getMembers();
-
-			System.out.println(findTeam.getMembers());
-			System.out.println(members);
-			System.out.println(findMember.getTeam().getId());
+			em.persist(movie);
 
 			tx.commit();
 		} catch (Exception e) {
